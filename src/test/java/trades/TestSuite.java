8c3e7c68ae4c89102414c2ee09e451e9dceff833
Shawn Wang <whh99u@gmail.com>
2022-05-12 05:25:33 +0800

    [Change] remove IO test

--- src/test/java/trades/TestSuite.java
+++ src/test/java/trades/TestSuite.java
@@ -4,18 +4,10 @@ import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 
 public class TestSuite {
-    //private final static Logger log = LoggerFactory.getLogger(TestSuite.class);
-
     // Test all test cases
     @Suite
     @SelectPackages("trades.testCase")
     public static class TestAllTestSuite {
-    }
-
-    // Test all io test cases
-    @Suite
-    @SelectPackages("trades.testCase.io")
-    public static class IOTestSuite {
     }
 
     // Test all consistency test cases
